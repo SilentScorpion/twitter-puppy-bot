d5991fce0c117f46964416cd9f1e699850593882
Ajay Kamath <ajaymanit20@gmail.com>
2019-09-24 22:18:23 +0530

    deleted 1

--- .gitignore/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key : 'ocXNPb0V4H4wKTaRv1VvzhwVH',
-    consumer_secret: 'EyYua6o4YEg2Ckkp1rIzstJlZTZl6B361Nq5OPp7KapqyJpsa1',
-    access_token: '1176498093186936833-Dv3DRF6B716uvvoa0nrPBoj0O97gVV',
-    access_token_secret: 'otizAgpyEmdieDTY7C4RHwbJRVgi0kJPeiXCbxJN4u2AR',
-}
